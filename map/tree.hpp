590ca2d815b401f50020385c8a99c3bde5e92bfe
Theo ARCHEN <tharchen@e1r10p17.42.fr>
2022-06-01 16:57:44 +0200

    tree balanceok :)

--- map/tree.hpp
+++ map/tree.hpp
@@ -233,6 +233,7 @@ namespace ft
                 bool islegal = true;
                 std::vector<node_pointer> v;
                 typename std::vector<node_pointer>::iterator it;
+                int     depths[7] = { 0 };
 
                 tree_to_vector(v, _root);
                 it = v.begin();
@@ -243,8 +244,25 @@ namespace ft
                         islegal = false;
                         std::cout << "unreachable node -> " << (*it)->value.first << std::endl;
                     }
+                    int d = get_node_depth_diff(*it);
+                         if (d <  -2) depths[0] += 1;
+                    else if (d == -2) depths[1] += 1;
+                    else if (d == -1) depths[2] += 1;
+                    else if (d ==  0) depths[3] += 1;
+                    else if (d ==  1) depths[4] += 1;
+                    else if (d ==  2) depths[5] += 1;
+                    else if (d >   2) depths[6] += 1;
                     it++;
                 }
+                std::cout << "[ <2 ][ -2 ][ -1 ][  0 ][ +1 ][ +2 ][ >2 ]" << std::endl;
+                std::cout << "["  << std::setw(4) << depths[0]
+                          << "][" << std::setw(4) << depths[1]
+                          << "][" << std::setw(4) << depths[2]
+                          << "][" << std::setw(4) << depths[3]
+                          << "][" << std::setw(4) << depths[4]
+                          << "][" << std::setw(4) << depths[5]
+                          << "][" << std::setw(4) << depths[6]
+                          << "]" << std::endl;
                 return (islegal);
             }
 
